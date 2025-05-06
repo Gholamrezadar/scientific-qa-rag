f0f763727843b853e514e8ad9072adf587be9872
Gholamreza Dar <rezadar1378@gmail.com>
2025-05-06 16:39:07 +0330

    fix rag.py functions

--- src/rag.py
+++ src/rag.py
@@ -4,12 +4,12 @@ import os
 
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
 
-from models import EMBEDDING_MODEL_NAME, GENERATION_MODEL_NAME
+from .models import EMBEDDING_MODEL_NAME, GENERATION_MODEL_NAME
 
-embedding_model = OllamaEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+embedding_model = OllamaEmbeddings(model=EMBEDDING_MODEL_NAME)
 vector_db = Chroma(collection_name='rag-db', persist_directory='rag_db', embedding_function=embedding_model)
 
 
@@ -34,7 +34,7 @@ def load_documents(doc_dir: str) -> List[str]:
     return documents
 
 
-def chunk_document(document: str, chunk_size: int = 500, chunk_overlap: int = 100) -> List[str]:
+def chunk_document(document: str, chunk_size: int = 1000, chunk_overlap: int = 200) -> List[str]:
     '''Divides the document into smaller chunks using langchain RecursiveTextSplitter.
 
     Args:
@@ -50,55 +50,57 @@ def chunk_document(document: str, chunk_size: int = 500, chunk_overlap: int = 10
     return splitter.split_text(document)
 
 
-def embed_chunks(chunks: List[str]) -> List[List[float]]:
-    '''Embeds the chunks using the specified embedding model.
+# def embed_chunks(chunks: List[str]) -> List[List[float]]:
+#     '''Embeds the chunks using the specified embedding model.
+
+#     Args:
+#         chunks (List[str]): The chunked documents.
+
+#     Returns:
+#         List[List[float]]: The embedded chunks.
+#     '''
+#     embedded_chunks = []
+#     for chunk in chunks:
+#         embedded_chunks.append(embedding_model.embed_query(chunk))
+#     return embedded_chunks
+
+
+# def embed_question(question: str) -> List[float]:
+#     '''Embeds the question using the specified embedding model.
+
+#     Args:
+#         question (str): The question to embed.
+
+#     Returns:
+#         List[float]: The embedded question.
+#     '''
+
+#     return embedding_model.embed_query(question)
+
+
+def store_chunks_in_db(chunks: List[str]) -> None:
+    '''Stores chunks in the database.
 
     Args:
-        chunks (List[str]): The chunked documents.
-
-    Returns:
-        List[List[float]]: The embedded chunks.
-    '''
-    embedded_chunks = []
-    for chunk in chunks:
-        embedded_chunks.append(embedding_model.embed_query(chunk))
-    return embedded_chunks
-
-
-def embed_question(question: str) -> List[float]:
-    '''Embeds the question using the specified embedding model.
-
-    Args:
-        question (str): The question to embed.
-
-    Returns:
-        List[float]: The embedded question.
+        chunks (List[str]): The chunks to store.
     '''
 
-    return embedding_model.embed_query(question)
+    vector_db.add_texts(texts=chunks)
 
-
-def store_vector_in_db(vectors: List[List[float]]) -> None:
-    '''Stores an embedded vector in a database.
-
-    Args:
-        vectors (List[List[float]]): The vectors to store.
-    '''
-
-    vector_db.add(vectors)
-    vector_db.persist()
-
-
-
-def retrieve_relevant_chunks(embedded_question: List[float], k: int = 2) -> List[str]:
+    
+def retrieve_relevant_chunks(question: str, k: int = 2) -> List[str]:
     '''Retrieves the relevant chunks from the database using the specified embedding model.
 
     Args:
-        embedded_question (List[float]): The embedded question.
+        question (str): The question to embed.
+        k (int, optional): The number of chunks to retrieve.
 
     Returns:
         List[str]: The relevant chunks.
     '''
+    retrieved_results = vector_db.similarity_search(query=question, k=k)
+    relevant_chunks: List[str] = []
+    for result in retrieved_results:
+        relevant_chunks.append(result.page_data)
+    return relevant_chunks
 
-    results = vector_db.similarity_search_by_vector(embedded_question, k=k)
-    return [doc.page_content for doc in results]
